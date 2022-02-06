2f27a2dde2538059c82ead7343497f398dd24873
Ivan López <ivan.lopez1@unmsm.edu.pe>
2022-02-05 20:33:51 -0500

    puertos y path

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -0,0 +1,22 @@
+package com.fundamentos.springboot.fundamentos.bean;
+
+
+
+
+public class MyBeanWithDependencyImplement implements  MyBeanWithDependency{
+
+    MyOperation myOperation;
+
+    public MyBeanWithDependencyImplement(MyOperation myOperation) {
+        this.myOperation = myOperation;
+    }
+
+    @Override
+    public void printWithDependency() {
+        int numero=1;
+        System.out.println(myOperation.suma(numero));
+        System.out.println("Hola desde un bean con dependencia");
+    }
+}
+
+
